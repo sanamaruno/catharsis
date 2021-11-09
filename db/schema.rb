aa812f6440ec3840dc6ef09d1f217a0c762f7496
sanamaruno <xwkpm269@gmail.com>
2021-11-09 10:09:08 +0000

    Add マイページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_034919) do
+ActiveRecord::Schema.define(version: 2021_11_09_093231) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_034919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
