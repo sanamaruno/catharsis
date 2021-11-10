8a91e5217566d130d5da0a04bf4491be888e05cf
sanamaruno <xwkpm269@gmail.com>
2021-11-10 11:02:17 +0000

    Add 新規投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,27 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    tag_list = params[:post][:tag_name].split(nil)
+    if @post.save
+      @post.save_tag(tag_list)
+      redirect_to post_path(@post.id)
+    else
+      redirect_to request.referer
+    end
   end
 
   def index
+    @tag_list = Tag.all
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @post_tags = @post.tags
   end
 
   def edit
@@ -21,6 +34,12 @@ class PostsController < ApplicationController
   end
 
   def search
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:place, :title, :body, :user_id)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,26 @@ class Post < ApplicationRecord
   has_many :tags, through: :tag_maps
   # throughオプションによってtag_mapsテーブルを通してtagsテーブルとの関連付けを行う
   # これによりPost.tagsとすればPostに紐付けられたTagの取得が可能になる
+
+  # 投稿を編集する際に以下一連の動作が必要になる
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    # createアクションで保存した@postに紐付くtagが存在する場合、
+    # 「タグの名前を配列として」全て取得する
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+    # 「現在取得した@postに存在するタグ」から「送信されてきたタグ」を除いたタグをold_tags
+    # 「送信されてきたタグ」から「現在存在するタグ」を除いたタグをnew_tags
+
+    # 古いタグの削除
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(tag_name: old)
+    end
+
+    # 新しいタグの保存
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(tag_name: new)
+      self.tags << new_post_tag
+    end
+  end
 end
