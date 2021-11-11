ebbb398f4bbcfd59f2ca0024e0f5db083b258b33
sanamaruno <xwkpm269@gmail.com>
2021-11-11 09:20:53 +0000

    Add 投稿詳細・編集・削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,21 +16,37 @@ class PostsController < ApplicationController
 
   def index
     @tag_list = Tag.all
-    @posts = Post.all
+    @posts = Post.all.reverse_order
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
     @post_tags = @post.tags
   end
 
   def edit
+    @post = Post.find(params[:id])
+    if @post.user == current_user
+      render :edit
+    else
+      redirect_to posts_path
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to user_path(current_user)
   end
 
   def search
